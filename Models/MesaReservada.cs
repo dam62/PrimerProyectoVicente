effad481a8f0f30c25011164b68979b8ad47cb1e
dam62 <dam62@gmail.com>
2025-09-29 17:20:58 +0200

    Actualizacion1

---
+++ Models/MesaReservada.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PrimerProyectoVicente.Models;
+
+public class MesaReservada
+{
+    public string evento { get; set; } = string.Empty;
+    public string cliente { get; set; } = string.Empty;
+    public int asientos { get; set; } = 0;
+    public DateTime fecha { get; set; } = DateTime.Today;
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,49 @@
-﻿namespace PrimerProyectoVicente.ViewModels;
+﻿using System.Collections.Generic;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PrimerProyectoVicente.Models;
+
+namespace PrimerProyectoVicente.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private string mensaje = string.Empty;
+    
+    [ObservableProperty] private bool condiciones = false;
+    
+    [ObservableProperty] private bool opcionesAvanzadas = false;
+    
+    [ObservableProperty] private List<MesaReservada> mesas = new();
+
+    [ObservableProperty] private MesaReservada mesa = new();
+    
+    public List<string> listaEventos { set; get; }
+    
+    public MainWindowViewModel()
+    {
+        ListarEventos();
+    }
+
+    [RelayCommand]
+    public void CargarMesas()
+    {
+        mesa = new MesaReservada();
+        
+    }
+    
+    [RelayCommand]
+    private void ListarEventos()
+    {
+        listaEventos = new()
+        {
+            "Cumpleaños", "Comunión", "Boda", "Fiesta"
+        };
+        Mesa.evento = listaEventos[0];
+    }
+
+    [RelayCommand]
+    public void CrearReserva()
+    {
+        mesa = new MesaReservada();
+    }
 }
