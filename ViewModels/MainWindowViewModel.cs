a6dbe48ab657f342fe3df57b9e5edbb3dc817e1d
dam62 <vicentemcr16@gmail.com>
2025-09-30 00:52:28 +0200

    modificaciones

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Avalonia.Controls;
+using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using PrimerProyectoVicente.Models;
@@ -7,15 +11,21 @@ namespace PrimerProyectoVicente.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    private string mensaje = string.Empty;
+    [ObservableProperty] private string mensaje = string.Empty;
     
     [ObservableProperty] private bool condiciones = false;
     
     [ObservableProperty] private bool opcionesAvanzadas = false;
-    
-    [ObservableProperty] private List<MesaReservada> mesas = new();
 
     [ObservableProperty] private MesaReservada mesa = new();
+    
+    [ObservableProperty] private bool modoEditar = false;
+    
+    [ObservableProperty] private bool modoCrear = true;
+    
+    [ObservableProperty] private MesaReservada mesaEditar = new ();
+    
+    [ObservableProperty] private ObservableCollection<MesaReservada> reservas = new();
     
     public List<string> listaEventos { set; get; }
     
@@ -23,15 +33,7 @@ public partial class MainWindowViewModel : ViewModelBase
     {
         ListarEventos();
     }
-
-    [RelayCommand]
-    public void CargarMesas()
-    {
-        mesa = new MesaReservada();
-        
-    }
     
-    [RelayCommand]
     private void ListarEventos()
     {
         listaEventos = new()
@@ -39,6 +41,60 @@ public partial class MainWindowViewModel : ViewModelBase
             "Cumpleaños", "Comunión", "Boda", "Fiesta"
         };
         Mesa.evento = listaEventos[0];
+    }
+    
+    [RelayCommand]
+    public void ComprobarFecha()
+    {
+        CheckDate();
+    }
+    
+    private bool CheckDate()
+    {
+        if (Mesa.fecha < DateTime.Today)
+        {
+            Mensaje = "La fecha no puede ser inferior a hoy";
+            return false;
+        }
+        else
+        {
+            Mensaje = string.Empty; //Mensaje = "";
+            return true;
+        }
+    }
+    
+    [RelayCommand]
+    public void EditarMesa()
+    {
+        Mesa = MesaEditar;
+        ModoCrear = false;
+        ModoEditar = true;
+    }
+
+    [RelayCommand]
+    public void MostrarOpcionesAvanzadas()
+    {
+        if (OpcionesAvanzadas)
+        {
+            OpcionesAvanzadas = false;
+        }
+        else
+        {
+            OpcionesAvanzadas = true;
+        }
+        
+    }
+
+    [RelayCommand]
+    public void EstadoInicialCheck(object parameter)
+    {
+        CheckBox check =  (CheckBox)parameter;
+
+        if (check.IsChecked == true)
+        {
+            check.Foreground = Brushes.Black;
+            check.FontWeight = FontWeight.Bold;
+        }
     }
 
     [RelayCommand]
